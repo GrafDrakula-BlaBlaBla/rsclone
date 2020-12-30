aa8c2565fc8ae01495b2b8b5e64f8960e44eb646
koverchik <koverchik.o@gmail.com>
2020-12-30 15:15:04 +0300

    add: new component in project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
+import Calendar from './Calendar/Calendar';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="main-section">
+        <Calendar/>
       </div>
       <Footer />
     </div>
---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './_Calendar.scss';
+
+export default function Calendar() {
+  return(
+    <div className="calendar">
+
+    </div>
+  );
+}
