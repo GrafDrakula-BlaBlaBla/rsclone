43e1905c43158d7b7a39da01cb8bc60505e17ce6
koverchik <koverchik.o@gmail.com>
2021-01-08 23:12:02 +0300

    add: view data events on month

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -11,51 +11,35 @@ import './_Calendar.scss';
 
 export default function Calendar() {
   let now = new Date();
-  let [gridMount, changeGridMount] = useState([]);
-  const [dataGrid, changeDataGrid] = useState([]);
-  dataGrid.length = 35;
 
 
    Date.prototype.daysInMonth = function() {
      return 33 - new Date(this.getFullYear(), this.getMonth(), 33).getDate();
    };
 
+   let [gridMount, changeGridMount] = useState([]);
+   // сетка месяца
+   const [dataGrid, changeDataGrid] = useState([]);
+   dataGrid.length = 35;
+   dataGrid.fill(null);
+
+   // сегодняшний день
   const [nowDay, setDay] = useState(now.getDate());
+  // месяц
   const [stateMouth, setMouth] = useState(now.getMonth());
+  // год
   const [stateYear, setYear] = useState(now.getFullYear());
+  // первый день месяца
   const firstDayForWeek = new Date(stateYear, stateMouth, 1);
+  // день недели первого дня месяца (старт данных)
   const [nowDayOfWeek, setDayOfWeek] = useState(firstDayForWeek.getDay());
+  // сколько дней в месяце
   const [nowDaysInMonth, setdaysInMonth] = useState(now.daysInMonth());
 
-  let position;
+
   let beforeData;
 
-// Создание календаря
-
- function createOneMounth(stateMouth, stateYear, nowDay, nowDayOfWeek, nowDaysInMonth) {
-   let arrayCalendar = [];
-   (nowDayOfWeek !== 0) ? position = nowDayOfWeek - 1 :  position = 7 ;
-
-   for (var i = 0; i < nowDaysInMonth; i++) {
-     dataGrid[position + i] = i + 1 ;
-
-   }
-   for (var i = 0; i < dataGrid.length; i++) {
-     let className = 'day ';
-     className += (nowDay === dataGrid[i]) ? 'now-day' : '';
-     let item =
-       <div className={className}  >
-        <div>{dataGrid[i]}</div>
-       </div>
-
-     arrayCalendar.push(item);
-   }
-   gridMount = arrayCalendar;
-
- }
-
- createOneMounth(stateMouth, stateYear, nowDay, nowDayOfWeek, nowDaysInMonth);
-
+ // Дни недели
   const daysForWeekElem = [];
 
   for (var i = 0; i < 7; i++) {
@@ -66,6 +50,19 @@ export default function Calendar() {
     daysForWeekElem.push(item);
   }
 
+// Создание структуры
+
+  function createOneMounth( nowDayOfWeek, nowDaysInMonth) {
+    let position;
+    (nowDayOfWeek !== 0) ? position = nowDayOfWeek - 1 :  position = 7 ;
+    // создание сетки дней (массив с пустыми индексами) СТРУКТУРА
+    for (var i = 0; i < nowDaysInMonth; i++) {
+      dataGrid[position + i] = i + 1 ;
+      }
+      return dataGrid;
+    }
+
+  createOneMounth( nowDayOfWeek, nowDaysInMonth);
 
  function arrowLeftCalendar() {
    stateMouth == 0 ? now = new Date(stateYear - 1, 11, 1) : now =  new Date(stateYear, stateMouth-1, 1);
@@ -74,65 +71,75 @@ export default function Calendar() {
    setDay(now.getDate());
    setDayOfWeek(now.getDay());
    setdaysInMonth(now.daysInMonth());
-   createOneMounth(stateMouth, stateYear, nowDay, nowDayOfWeek, nowDaysInMonth);
+   createOneMounth( nowDayOfWeek, nowDaysInMonth);
  }
 
  function arrowRightCalendar() {
+
    stateMouth == 11 ? now = new Date(stateYear + 1, 1, 1) : now =  new Date(stateYear, stateMouth + 1, 1);
    setMouth(now.getMonth());
    setYear(now.getFullYear());
    setDay(now.getDate());
    setDayOfWeek(now.getDay());
    setdaysInMonth(now.daysInMonth());
-   createOneMounth(stateMouth, stateYear, nowDay, nowDayOfWeek, nowDaysInMonth);
+   createOneMounth( nowDayOfWeek, nowDaysInMonth);
  }
 
-  arrayDataEvents.then((data) => {
-
-    function createEventsElem(indexDataGrid) {
-      let arrayEventsOneDayCaltnlar = [];
-      for (var i = 0; i < data[indexDataGrid].length; i++) {
-        let item =
-            <div>
-              { data[indexDataGrid][i]['sammary'] }
-            </div>
-            arrayEventsOneDayCaltnlar.push(item);
-          }
-        return arrayEventsOneDayCaltnlar;
-    }
-    dataGrid.map( ( item, index ) => {
-        if(data.hasOwnProperty(index)){
-          dataGrid[index] = <div class="day "> {index} { createEventsElem(index) } </div>;
-        }else{
-          dataGrid[index] = <div> {item} </div>
-        }
-    })
-
-    console.log(dataGrid);
-    changeDataGrid(dataGrid);
-
-    gridMount = dataGrid;
-
-    // console.log(dataGrid);
-    // console.log("reques");
-  })
+function changeMounth(data) {
+    changeGridMount(data);
+    console.log("hello");
+}
 
 useEffect(() => {
+  // Данные пришли => надеть структуру на данные
+    arrayDataEvents.then((data) => {
 
+      function createEventsElem(indexDataGrid) {
+        let arrayEventsOneDayCaltnlar = [];
+        for (var i = 0; i < data[indexDataGrid].length; i++) {
+          let item =
+              <div>
+                { data[indexDataGrid][i]['sammary'] }
+              </div>
+              arrayEventsOneDayCaltnlar.push(item);
+            }
+          return arrayEventsOneDayCaltnlar;
+      }
 
-}, [gridMount])
+      // Перебор структуры
+        let wrapperData = dataGrid.map(( item, index) => {
+          let className = 'day ';
+           if(data.hasOwnProperty(index)){
+
+             dataGrid[index] = <div className={ className }> {index} { createEventsElem(index) } </div>;
+              }else{
+                let itemWithoutData =
+                  <div className={ className }>
+                    <div>{ item }</div>
+                  </div>
+              dataGrid[index] = itemWithoutData;
+            }
+            return dataGrid[index];
+        })
+        changeMounth(wrapperData);
+    })
+}, [])
 
   return(
     <div className="calendar">
-    <div className="wrapper-calendar-mounth">
-      <div className="head-calendar"> <div className="button-click-calendar" onClick={arrowLeftCalendar}>&#129152;</div>
-      {nameMonth[stateMouth]} { stateYear }<div className="button-click-calendar" onClick={arrowRightCalendar}>&#129154;</div></div>
-      <div className="wrapper-day-for-week">{daysForWeekElem}</div>
-      <div className="wrapper-day-calendar">
-        {gridMount}
+      <div className="wrapper-calendar-mounth">
+        <div className="head-calendar">
+          <div className="button-click-calendar" onClick={arrowLeftCalendar}>&#129152;</div>
+          {nameMonth[stateMouth]} { stateYear }
+          <div className="button-click-calendar" onClick={arrowRightCalendar}>&#129154;</div>
+        </div>
+        <div className="wrapper-day-for-week">{daysForWeekElem}</div>
+        <div className="wrapper-day-calendar">
+          {gridMount}
+        </div>
       </div>
-    </div>
       <OneDay mouth={stateMouth} day={nowDay}/ >
     </div>
+
   );
 }
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import './_Map.scss';
-import { ReactBingmaps } from 'react-bingmaps';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 export default function Map() {
-
-  const YourBingMapsKey = 'ApTgQsA_1ru04YR2DpZBe2xVSpOrYI9WJJDuMLisZu-MryfrveshaFSkyHqYlInN';
+  const position = [53, 28];
+  const zoom = 2
   return(
     <div className="map-contener">
-    <ReactBingmaps
-      bingmapKey = {YourBingMapsKey}
-      center = {[53.9022, 27.5618]}
-      zoom = {6.5}>
-    </ReactBingmaps>
+
     </div>
   );
 }
