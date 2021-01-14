d2165c295dc34e84ec9eb0a8decee8fad95738a2
koverchik <koverchik.o@gmail.com>
2021-01-14 14:58:20 +0300

    add: stage node "one day" and don't working version "calendar month"

---
+++ src/components/Calendar/store/StateCalendarMonth.jsx
@@ -0,0 +1,122 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { makeAutoObservable } from "mobx"
+import { observer } from "mobx-react-lite"
+import arrayDataAllEvents from './../arrayAllTime.jsx'
+
+Date.prototype.daysInMonth = function() {
+  return 33 - new Date(this.getFullYear(), this.getMonth(), 33).getDate();
+};
+
+class StateCalendarMonth {
+
+  dayOfWeek = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+
+  now = new Date();
+  nowDay = this.now.getDate();
+  stateMouth = this.now.getMonth();
+  stateYear = this.now.getFullYear();
+  nowDaysInMonth = this.now.daysInMonth();
+  firstDayForWeek = new Date(this.stateYear, this.stateMouth, 1);
+  nowDayOfWeek = this.firstDayForWeek.getDay();
+  gridMount = [];
+  dataGrid = new Array(35);
+  wrapperData = null;
+
+   constructor() {
+       makeAutoObservable(this)
+   }
+
+  createDayOfWeek(){
+    const daysForWeekElem = [];
+
+    for (var i = 0; i < 7; i++) {
+      let item =
+      <div className="day-of-week" >
+        <div > {this.dayOfWeek[i]}</div>
+      </div>
+      daysForWeekElem.push(item);
+    }
+    return daysForWeekElem;
+  }
+
+    // Создание элемента мероприятия
+    createEventsElem(indexDataGrid, data) {
+      let arrayEventsOneDayCaltnlar = [];
+      for (var i = 0; i < data[indexDataGrid].length; i++) {
+        let idEvent = data[indexDataGrid][i]["id"];
+
+        let item =
+            <button className="one-event-calendar btn btn-primary" type="button" data-toggle="modal" data-target= { idEvent }>
+              {data[indexDataGrid][i]['sammary'] }
+            </button>
+            arrayEventsOneDayCaltnlar.push(item);
+          }
+        return arrayEventsOneDayCaltnlar;
+    }
+
+      createOneMounth() {
+
+        function createGridOneMounth(nowDayOfWeek, nowDaysInMonth, dataGrid) {
+               let position;
+               (nowDayOfWeek !== 0) ? position = nowDayOfWeek - 1 :  position = 7 ;
+               // создание сетки дней (массив с пустыми индексами) СТРУКТУРА
+               for (var i = 0; i < nowDaysInMonth; i++) {
+
+                 dataGrid[position + i] = i + 1 ;
+                 }
+              return dataGrid;
+               }
+
+
+                arrayDataAllEvents.then(( data ) => {
+
+                let newGridForMonth = createGridOneMounth(this.nowDayOfWeek, this.nowDaysInMonth, this.dataGrid);
+
+                this.wrapperData = newGridForMonth.map(( item, index) => {
+                 // Перебор структуры
+                 let className = 'day';
+                 // console.log(data);
+                 if(data.hasOwnProperty(this.stateYear)){
+
+                   if(data[this.stateYear].hasOwnProperty(this.stateMouth)){
+
+                     if(data[this.stateYear][this.stateMouth].hasOwnProperty(item)){
+
+                       newGridForMonth[index] = <div className={ className }>
+                       <div className="namber-day"> { item } </div> { this.createEventsElem(item, data[this.stateYear][this.stateMouth]) } </div>;
+
+                     } else {
+                       let itemWithoutData =
+                       <div className={ className } >
+                       <div className="namber-day">{ item } </div>
+                       </div>
+                       newGridForMonth[index] = itemWithoutData;
+                     }
+                      return newGridForMonth[index];
+
+                   } else {
+                     let itemWithoutData =
+                     <div className= { className } >
+                     <div className="namber-day">{ item } </div>
+                     </div>
+                     newGridForMonth[index] = itemWithoutData;
+                     return newGridForMonth[index];
+                   }
+                 } else {
+                   let itemWithoutData =
+                   <div className= { className } >
+                   <div className="namber-day">{ item } </div>
+                   </div>
+                   newGridForMonth[index] = itemWithoutData;
+                   return newGridForMonth[index];
+                 }
+               })
+               return newGridForMonth;
+             })
+
+           }
+
+  }
+
+export default new StateCalendarMonth()
