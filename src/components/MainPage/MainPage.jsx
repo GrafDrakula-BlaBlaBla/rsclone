439b94f4d0a2e5d8799d7abbe91bf1195684792a
koverchik <koverchik.o@gmail.com>
2021-01-16 14:07:59 +0300

    add: map on main page

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './_MainPage.module.scss';
 import MainSection from './MainSection/MainSection';
+import MapMain from './../MapMain/Map'
 
 export default function MainPage() {
   return (
     <div className={ styles.page }>
-      <div className={ styles.left_section }>MAP</div>
+      <div className={ styles.left_section }><MapMain /></div>
       <div className={ styles.right_section }>
         <MainSection />
       </div>
