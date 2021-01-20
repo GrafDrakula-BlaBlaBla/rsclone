fbcc28d79d80de2999378d7243d26a58c09e6d4c
Aleksandr <foxi4554@gmail.com>
2021-01-20 20:57:33 +0300

    feat: implemented drag & drop

--- src/components/GamePage/Game/GameComponent.jsx
+++ src/components/GamePage/Game/GameComponent.jsx
@@ -1,27 +1,35 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './_GameComponent.module.scss'
 import Game from './Game';
 
 const GameComponent = () => {
   const gameWrapper = useRef(null);
   const gameContainer = useRef(null);
+  // const [ gameInit, setGameInit ] = useState(false);
 
   function changeScreenMode() {
     gameWrapper.current.classList.toggle( styles.full_screen );
+
+    if(game) {
+      game.resizeWindow();
+      game.render();
+    }
   }
 
-  function startGame(event) {
-    const game = new Game(gameContainer.current, gameWrapper.current);
-
-    event.target.style.display = 'none';
-
+  function initGame() {
+    game = new Game(gameContainer.current, gameWrapper.current);
     game.init();
   }
+
+  let game = null;
 
   return (
     <div ref={ gameWrapper } className={ styles.game_wrapper }>
       <button className={ styles.full_screen_btn } onClick={ changeScreenMode }></button>
-      <button className={ styles.start_btn } onClick={ startGame }>Начать игру</button>
+      <button className={ styles.start_btn } onClick={ (event) => {
+        event.target.style.display = 'none';
+        initGame();
+      }}>Начать игру</button>
       <canvas ref={ gameContainer } className={ styles.game_container }></canvas>
     </div>
   );
