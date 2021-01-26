1630e65fa1677c19d48edf415c32e85152c6618c
koverchik <koverchik.o@gmail.com>
2021-01-27 00:00:52 +0300

    add: file for request google auth

---
+++ server/routes/auth/googleAuthorization.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import User from "../../models/User";
+import * as bcrypt from "bcrypt";
+import * as jwt from "jsonwebtoken";
+import * as config from "config";
+
+const router = Router();
+
+router.post("/authentication-google", async (req, res) => {
+  try {
+    return res.json({ message: "Work!" });
+  } catch (e) {
+    return res.json({ message: "Don't work!" });
+  }
+});
+
+export default router;
---
+++ src/components/RegModal/authorizationGoogle/autoriztionGoogle.jsx
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export default function signIn() {
+  axios.get('/user', {
+      params: {
+        ID: 12345
+      }
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+}
