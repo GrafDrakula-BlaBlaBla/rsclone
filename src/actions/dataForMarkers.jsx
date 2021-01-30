a85e8bfeef2cee45afa34cf7e0377238fcdb16c3
Aleksandr <foxi4554@gmail.com>
2021-01-30 09:19:12 +0300

    fix

--- src/actions/dataForMarkers.jsx
+++ src/actions/dataForMarkers.jsx
@@ -2,7 +2,6 @@ import resultFetch from  './RequestCalendar.jsx';
 import axios from 'axios';
 // слияние двух массивов данных
 
-<<<<<<< HEAD
 const  dataMarker = resultFetch.then( ( arrayData )=>{
 // перебор данных (списка) пришедших из базы
 const result = axios.post('http://localhost:8000/create-map-main', {
@@ -28,30 +27,5 @@ const result = axios.post('http://localhost:8000/create-map-main', {
       });
 return result;
 })
-=======
-const  dataMarker = resultFetch.then( ( arrayData ) => {
-  // перебор данных (списка) пришедших из базы
-  const result = axios.post('http://localhost:8000/create-map-main', {})
-  .then(function (response) {
-    if(response.status === 200){
-      return Array.from(response.data);
-    }
-  })
-  .catch(function (error) {})
-  .then((dataDB) => {
-    const concatArray = dataDB.map( ( element ) => {
-      for (let i = 0; i < arrayData.length; i++) {
-        if(arrayData[i]['id'] === element.idEventCalendarGoogle){
-          const result = Object.assign(element, arrayData[i]);
-          return result;
-        }
-      }
-    });
-    return concatArray;
-  });
-
-  return result;
-});
->>>>>>> feature/game-page
 
 export default dataMarker;
