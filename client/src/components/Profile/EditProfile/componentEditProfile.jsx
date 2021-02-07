1a439eeabb5b5a29f007e02667454485c7da4d5c
koverchik <koverchik-work@yandex.ru>
2021-02-08 00:16:24 +0300

    add: structure edit profile

---
+++ client/src/components/Profile/EditProfile/componentEditProfile.jsx
@@ -0,0 +1,98 @@
+import React, { useEffect, setState, useRef, useState } from 'react';
+import './../_Profile.scss';
+import styles from './_EditProfile.scss';
+import SectionWrapper from '../../SectionWrapper/SectionWrapper';
+import RatingLeaves from '../../RatingLeaves/RatingLeaves';
+import { observer, inject } from "mobx-react";
+import byCities from "../../../modules/data/by-cities";
+
+const EditProfile = inject( "store" ) (observer( ({store}) => {
+
+
+
+useEffect(() => {
+
+  store.User.getIdinLocalStore();
+  store.User.getValue(store.User.id);
+
+}, [ store.User ])
+
+
+  const city = byCities['regions'][0]['cities'].map(( item, index ) => {
+    return <option value={index} key={"region" +index}> {item.name} </option>
+  })
+
+
+
+const [region, setRegion] = useState(0);
+const [cities, setСities] = useState(city);
+
+useEffect(() => {
+
+  function creteCities(i) {
+
+    const city = byCities['regions'][i]['cities'].map(( item, index ) => {
+      return <option value={index} key={"region" +index}> {item.name} </option>
+    })
+
+    return city;
+  }
+
+setСities(creteCities(region))
+}, [ region ])
+
+function onloadFile() {
+
+}
+
+
+  return (
+    <div className="profile-page">
+      <div className="events-history">
+            <div className="sex-user">
+              <p>Пол</p>
+              <select>
+                <option>не выбран</option>
+                <option>женский</option>
+                <option>мужской</option>
+              </select>
+            </div>
+            <div className="location-block-user">
+              <div className="location-user">
+                <p>Область</p>
+                <select onChange={(e) => setRegion(e.target.value) }
+                >
+                {
+                  byCities['regions'].map(( item, index ) => {
+                    return <option value={index} key={"region" +index}> {item.name} </option>
+                  })
+                }
+                </select>
+              </div>
+              <div className="location-user">
+                <p>Город</p>
+                <select  >
+                { cities }
+                </select>
+              </div>
+            </div>
+            <div className="contacts-user">
+              <p>Контакты</p>
+              <button className="button-contacts-user"><i class="fa fa-plus" aria-hidden="true"></i></button>
+            </div>
+            <div className="change-pic">
+            <p>Сменить фото</p>
+              <div style={styles}>
+                <label className="custom-file-upload" >
+                  <input type="file" accept="image/*" onChange={onloadFile} />
+                  <i className="fa fa-cloud-upload" aria-hidden="true" /> Загрузить
+                </label>
+                </div>
+            </div>
+          </div>
+
+    </div>
+    );
+  })
+)
+export default EditProfile;
